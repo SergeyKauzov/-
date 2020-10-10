4ed326a24e935cc23605bd7d6f8515ab24a6e22e
SergeyKauzov <70883593+SergeyKauzov@users.noreply.github.com>
2020-10-10 23:00:17 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-name = 'Каузов Сергей Сергеевич'
-age = 18
-city = 'Атырау'
-group = 'АиУ-19 р/о'
-print("ФИО: " +name+ ". Возраст: " +str(age)+ " лет. " "Город: "+city+ ". Группа: " +group+".")
-
-name = 'Каузов Сергей Сергеевич'
-age = 18
-city = 'Атырау'
-group = 'АиУ-19 р/о'
-print("\nФИО: " +name+ ". \nВозраст: " +str(age)+ " лет. \nГород: "+city+ ". \nГруппа: " +group+".")
+print('Введите глубину ёлки:')
+stars = int(input())
+star = 1
+while star < stars+1:
+    print(star * '*')
+    star = star + 1
+for star in range (3):
+    print('*' '*')
